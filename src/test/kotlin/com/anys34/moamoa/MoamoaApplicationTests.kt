3b310bc8f46c200b3351986de8e448aec6d6cc12
anys34 <anyeseong34@gmail.com>
2024-08-15 14:18:06 +0900

    init

---
+++ src/test/kotlin/com/anys34/moamoa/MoamoaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.anys34.moamoa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoamoaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
